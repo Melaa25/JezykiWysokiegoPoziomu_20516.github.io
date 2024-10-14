3ec56c8c5070a1bb9efe7cfce0cd9bab026d20af
Emilia Massowa <emilia.massowa@gmail.com>
2024-10-14 11:11:03 +0200

    Lab 3
    Zad 4

--- MassowaEmiliaLabJS/index.js
+++ MassowaEmiliaLabJS/index.js
@@ -1,26 +1,23 @@
 
-//Zad 3
+//Zad 4
+import dedent from "dedent";
 
-const http=require('http');
-const path= require("path");
-const util= require("util");
-const v8= require("v8");
+function Przyklad() {
+	const first = dedent`Nic dwa razy się nie zdarza
+                                i nie zdarzy. Z tej przyczyny
+    zrodziliśmy się bez wprawy
+                            i pomrzemy bez rutyny.`;
 
-const hostname='127.0.0.1';
-const port = 8888;
-
-const server= http.createServer((request,response)=>{
-    response.statusCode=200;
-    response.setHeader('Constent-Type','text/plain');
-    response.end('Hello, from node.js serwer!\n');
-});
-
+	const second = dedent`
+    Choćbyśmy uczniami byli
+         najtępszymi w szkole świata,
+             nie będziemy repetować
+                żadnej zimy ani lata. 
+  `;
 
 
-server.listen(port,hostname,()=>{
-    util.log(v8.getHeapStatistics());
-    console.log(path.basename(__filename));
-    util.log(path.join(__dirname,'uploads','images'));
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+	return first + "\n\n" + second + "\n\n";
+}
 
+console.log(Przyklad());
+console.log("Moduł dzieli długiego stinga na kilka lini zgodnie z tym jak jest napisany w kodzie. Dzięki czemu łatwiej jest odczytywać długie stringi.");
