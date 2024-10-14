40ae642d1da2d32fbda78b783a5e0ae05f07571b
Emilia Massowa <emilia.massowa@gmail.com>
2024-10-14 11:08:42 +0200

    Lab 3
    Zad 2

--- MassowaEmiliaLabJS/index.js
+++ MassowaEmiliaLabJS/index.js
@@ -1,10 +1,21 @@
-//Zad 1 
+//Zad 2
+/*
+const fs = require("fs");
+const data = fs.readFileSync('package.json');
 
-const wait = 3000;
-console.log("Początek skryptu...");
-setTimeout(function(){
-    console.log("Upłynęły minimum 3 sek...")
-}, wait);
-console.log("koniec skryptu...");
+console.log("Początek skryptu.");
+console.log(data.toString());
+console.log("Koniec skryptu.");
+*/
 
+const fs = require("fs");
+
+console.log("Początek skryptu.");
+
+fs.readFile('package.json', function(err,data){
+    if(err) return console.error(err);
+    console.log(data.toString());
+});
+
+console.log("Koniec skryptu.");
 
