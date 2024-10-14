bd5211b7c8c749817462e3608130eff3a8421215
Emilia Massowa <emilia.massowa@gmail.com>
2024-10-14 11:07:10 +0200

    Lab 3
    Zad1

---
+++ MassowaEmiliaLabJS/index.js
@@ -0,0 +1,10 @@
+//Zad 1 
+
+const wait = 3000;
+console.log("Początek skryptu...");
+setTimeout(function(){
+    console.log("Upłynęły minimum 3 sek...")
+}, wait);
+console.log("koniec skryptu...");
+
+
